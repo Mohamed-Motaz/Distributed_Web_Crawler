06495c99e56587a6d7369077710eef6bcd60fe65
Mohamed247 <mohamedmotaz247@yahoo.com>
2022-02-13 14:10:36 +0200

    setup skeleton for db communication

--- Server/LockServer/Database/main.go
+++ Server/LockServer/Database/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	logger "Server/Cluster/Logger"
 	dbConfig "Server/LockServer/Database/Configurations"
+	"fmt"
 
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -23,4 +26,66 @@ type Info struct{
 }
 type DBWrapper struct{
 	db *gorm.DB
+}
+
+func main(){
+	logger.LogInfo(logger.DATABASE, "Starting setup of db")
+	dBWrapper := New()
+
+	dBWrapper.Close()
+}
+
+//return a thread-safe *gorm.DB that can safely be used 
+//by multiple goroutines
+func New() *DBWrapper{
+	db := connect()
+	setUp(db)
+	logger.LogInfo(logger.DATABASE, "Db setup complete")
+	return &DBWrapper{
+		db: db,
+	}
+	
+}
+
+func (db *DBWrapper) Close(){
+}
+
+func (db *DBWrapper) GetRecord(info *Info, query string, value interface{}){
+	db.db.First(info, query, value)
+}
+
+func (db *DBWrapper) GetRecords(infos *[]Info, query string, value interface{}){
+	db.db.Where(query, value).Find(infos)
+}
+
+func (db *DBWrapper) GetAllRecords(infos *[]Info){
+	db.db.Find(infos)
+}
+
+func (db *DBWrapper) AddRecord(info *Info){
+	db.db.Create(info)
+}
+
+func (db *DBWrapper) UpdateRecord(info *Info, data map[string]interface{} ){
+	db.db.Model(&info).Updates(data)
+}
+
+func connect() *gorm.DB{
+
+	dsn := fmt.Sprintf(
+		"user=%v password=%v host=%v port=%v database=%v sslmode=disable",
+		user, password, host, port, dbname)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil{
+		logger.FailOnError(logger.DATABASE, "Unable to connect to db with this error %v", err)
+	}
+	return db
+}
+
+func setUp(db *gorm.DB) {
+	err := db.AutoMigrate(&Info{})
+	if err != nil{
+		logger.FailOnError(logger.DATABASE, "Unable to migrate the tables with this error %v", err)
+	}
 }
