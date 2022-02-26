d2bdd40b4fad24ed42f8464e3a436edde508a2af
Mohamed247 <mohamedmotaz247@yahoo.com>
2022-02-26 18:37:23 +0200

    testclients changes

--- TestClients/index.js
+++ TestClients/index.js
@@ -1,26 +1,38 @@
 const WebSocket = require("ws");
 let conns = [];
+let ctr = 0;
 function connect(id) {
   let ws = new WebSocket("ws://127.0.0.1:8080/");
-  //conns.push(ws);
-  ws.onmessage = (m) =>
+  conns.push(ws);
+  ws.onmessage = (m) => {
     console.log("Received message " + JSON.stringify(JSON.parse(m.data)));
+    ctr++;
+    ws.close();
+  };
   ws.onopen = function (e) {
     console.log("[open] Connection established");
     console.log("Sending to server");
     ws.send(
       JSON.stringify({
         jobId: "JOB" + id,
-        urlToCrawl: "https://www.instagram.com/",
+        urlToCrawl: "https://www.reddit.com/",
         depthToCrawl: 1,
       })
     );
-    ws.close();
   };
 }
-for (let i = 1; i <= 500; i++) {
+for (let i = 1; i <= 100; i++) {
   connect(i);
-  (async () => await Promise.resolve(setTimeout(() => {}, 50)))();
+  //   (async () => await Promise.resolve(setTimeout(() => {}, 50)))();
 }
 
-(async () => await Promise.resolve(setTimeout(() => {}, 1000 * 1000)))();
+for (let i = 1; i < 100; i++) {
+  (async () =>
+    await Promise.resolve(
+      setTimeout(() => {
+        console.log(ctr);
+      }, 10 * 1000)
+    ))();
+}
+
+console.log(ctr);
